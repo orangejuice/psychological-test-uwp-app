3c115011108bf4054733e8d2aafccfe32c0c7f8d
蓅星 <cherrymeteor@outlook.com>
2018-03-24 17:01:22 +0800

    添加项目文件。

---
+++ bs-universal-app/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Windows.UI.Xaml;
+using System.Threading.Tasks;
+using bs_universal_app.Services.SettingsServices;
+using Windows.ApplicationModel.Activation;
+using Template10.Mvvm;
+using Template10.Common;
+using System.Linq;
+using Windows.UI.Xaml.Data;
+
+namespace bs_universal_app
+{
+    /// Documentation on APIs used in this page:
+    /// https://github.com/Windows-XAML/Template10/wiki
+
+    [Bindable]
+    sealed partial class App : BootStrapper
+    {
+        public App()
+        {
+            InitializeComponent();
+            SplashFactory = (e) => new Views.Splash(e);
+
+            #region app settings
+
+            // some settings must be set in app.constructor
+            var settings = SettingsService.Instance;
+            RequestedTheme = settings.AppTheme;
+            CacheMaxDuration = settings.CacheMaxDuration;
+            ShowShellBackButton = settings.UseShellBackButton;
+
+            #endregion
+        }
+
+        public override async Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
+        {
+            // TODO: add your long-running task here
+            await NavigationService.NavigateAsync(typeof(Views.MainPage));
+        }
+    }
+}
