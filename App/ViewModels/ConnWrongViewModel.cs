8a3c71d47bfe409c3265187c0f105122bafbf5b0
meteor <cherrymeteor@outlook.com>
2019-03-29 13:16:34 +0000

    archive

---
+++ App/ViewModels/ConnWrongViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using GalaSoft.MvvmLight;
+
+namespace App.ViewModels
+{
+    public class ConnWrongViewModel : ViewModelBase
+    {
+        public Uri WebViewURIsource = new Uri("ms-appx-web:///Assets/Game/index.html");
+        public ConnWrongViewModel()
+        {
+        }
+    }
+}
---
+++ App/Views/ConnWrongPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using App.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace App.Views
+{
+    public sealed partial class ConnWrongPage : Page
+    {
+        private ConnWrongViewModel ViewModel
+        {
+            get { return DataContext as ConnWrongViewModel; }
+        }
+
+        public ConnWrongPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
